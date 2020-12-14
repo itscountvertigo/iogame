765759d98a8d26db095d251a29353f65ce4acb98
Anbiya Popal <anbiya2004@gmail.com>
2020-12-14 21:36:37 +0100

    delete unused file

--- public/eat.js
+++
@@ -1,8 +0,0 @@
-function eat(eaterX, eaterY, eaterRadius, eatenX, eatenY, eatenRadius){
-  if (dist(eatenX, eatenY, eaterX, eatenY) < eaterRadius && eaterRadius > eatenRadius) {
-    var newRadius = sqrt((((((eaterRadius / 2) * (eaterRadius / 2)) * Math.PI) + ((eatenRadius / 2) * (eatenRadius / 2)) * Math.PI)) / Math.PI) * 2; // increase size by volume
-    return newRadius;
-  } else {
-    return null;
-  }
-}
